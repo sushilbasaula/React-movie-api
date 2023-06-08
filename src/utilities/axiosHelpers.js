f219eda2f07e8a452d5c17821fab1506eca6c0ec
Sushil Basaula <112741777+sushilbasaula@users.noreply.github.com>
2023-06-09 03:16:22 +1000

    added environment variable

--- src/utilities/axiosHelpers.js
+++ src/utilities/axiosHelpers.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const fetchData = (str) => {
-  const apiEp = "https://www.omdbapi.com/?apikey=4d4d632d&t=" + str;
+  const apiEp = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&t=${str}`;
   const response = axios(apiEp);
 
   return response;
